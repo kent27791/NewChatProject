a929d4daef67509613d01df77eda5a47f35b1393
cuongpc <cuongpc@CUONGPC>
2018-01-12 10:45:19 +0700

    create_10
    create_10

--- src/Chat.App/src/app/app.module.ts
+++ src/Chat.App/src/app/app.module.ts
@@ -1,24 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule  } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PageNotFoundComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
+    BrowserModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
